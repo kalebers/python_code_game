ec66b9049ee2c16a46916eff9ceba670f74075c5
Kalebe Szlachta <39911310+kalebers@users.noreply.github.com>
2024-04-18 13:49:02 +0200

    Update binary_unicode.py

--- binary_unicode.py
+++ binary_unicode.py
@@ -1,3 +1,26 @@
+"""
+Binary with 0 and 1 is good, but binary with only 0, or almost, is even better! Originally, this is a concept designed by Chuck Norris to send so called unary messages.
+Write a program that takes an incoming message as input and displays as output the message encoded using Chuck Norris’ method.
+Encoding principle:
+The input message consists of ASCII characters (7-bit)
+The encoded output message consists of blocks of 0
+A block is separated from another block by a space
+Two consecutive blocks are used to produce a series of same value bits (only 1 or 0 values):
+- First block: it is always 0 or 00. If it is 0, then the series contains 1, if not, it contains 0
+- Second block: the number of 0 in this block is the number of bits in the series
+Several tests show this code is proper:
+ 1) input: C
+    output: 0 0 00 0000 0 00
+ 2) input: CC
+    output: 0 0 00 0000 0 000 00 0000 0 00
+ 3) input: %
+    output: 00 0 0 0 00 00 0 0 00 0 0 0
+ 4) input: Chuck Norris' keyboard has 2 keys: 0 and white space
+    output: 0 0 00 0000 0 0000 00 0 0 0 00 000 0 000 00 0 0 0 00 0 0 000 00 000 0 0000 00 0 0 0 00 0 0 00 00 0 0 0 00 00000 0 0 00 00 0 000 00 0 0 00 00 0 0 0000000 00 00 0 0 00 0 0 000 00 00 0 0 00 0 0 00 00 0 0 0 00 00 0 0000 00 00 0 00 00 0 0 0 00 00 0 000 00 0 0 0 00 00000 0 00 00 0 0 0 00 0 0 0000 00 00 0 0 00 0 0 00000 00 00 0 000 00 000 0 0 00 0 0 00 00 0 0 000000 00 0000 0 0000 00 00 0 0 00 0 0 00 00 00 0 0 00 000 0 0 00 00000 0 00 00 0 0 0 00 000 0 00 00 0000 0 0000 00 00 0 00 00 0 0 0 00 000000 0 00 00 00 0 0 00 00 0 0 00 00000 0 00 00 0 0 0 00 0 0 0000 00 00 0 0 00 0 0 00000 00 00 0 0000 00 00 0 00 00 0 0 000 00 0 0 0 00 00 0 0 00 000000 0 00 00 00000 0 0 00 00000 0 00 00 0000 0 000 00 0 0 000 00 0 0 00 00 00 0 0 00 000 0 0 00 00000 0 000 00 0 0 00000 00 0 0 0 00 000 0 00 00 0 0 0 00 00 0 0000 00 0 0 0 00 00 0 00 00 00 0 0 00 0 0 0 00 0 0 0 00 00000 0 000 00 00 0 00000 00 0000 0 00 00 0000 0 000 00 000 0 0000 00 00 0 0 00 0 0 0 00 0 0 0 00 0 0 000 00 0
+"""
+
+
+
 def encode_binary(message):
     binary_message = ''.join(format(ord(char), 'b') for char in message) # Convert each character to its binary representation
     encoded_message = binary_message.replace('1', '0') # Replace all '1's with '0's
